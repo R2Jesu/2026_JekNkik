f277557439fbe6e91ee51ca39b4c957af14caa6a
R2Jesu <r2Jesu2019@gmail.com>
2026-03-14 19:42:11 -0500

    3/14 edits

--- src/main/java/frc/robot/subsystems/R2Jesu_IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/R2Jesu_IntakeSubsystem.java
@@ -6,19 +6,28 @@ package frc.robot.subsystems;
 
 
 
-import edu.wpi.first.hal.PWMConfigDataResult;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.networktables.GenericEntry;
+import java.util.Map;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+// use the revrobotics encoder in a closed loop instead of two PIDs
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
 
 
 public class R2Jesu_IntakeSubsystem extends SubsystemBase {
@@ -26,21 +35,50 @@ public class R2Jesu_IntakeSubsystem extends SubsystemBase {
   private SparkMax intake_armMotor = new SparkMax(51, MotorType.kBrushless);
   private SparkMax intake_wheelsMotor = new SparkMax(54, MotorType.kBrushless);
   private Encoder intake_armEncoder = new Encoder(0,1, true, CounterBase.EncodingType.k4X);
+
+// use the revrobotics encoder in a closed loop for the wheels
+  private final SparkClosedLoopController intake_wheelController = intake_wheelsMotor.getClosedLoopController();
+  // feed-forward for wheel velocity controller
+  // default wheel RPM to use when lowering intake (change to your robot's target)
+  private static final double intake_lower_wheel_rpm = 4500.0;
+  // Shuffleboard entry for a number slider to let user change intake RPM (0-9000)
+  private GenericEntry intakeRpmEntry;
+ // added to use closed loop controller
+  
   private static int targetPosition=0;
-  private double intake_armPositions[] = {0.0, 5000.0};
+  private double intake_armPositions[] = {0.0, 5300.0};
   private PIDController m_armUpController = new PIDController(.00025, 0.0, 0.0, 0.01); //p 1.5
   private PIDController m_armDownController = new PIDController(.00025, 0.0, 0.0, 0.01); //p 1.5
   private double up_pidOutput;
   private double down_pidOutput;
-  private PWMConfigDataResult myResult; //no idea
+  private double rpm=0;
  
 
  
   /** Creates a new R2Jesu_IntakeSubsystem. */
 
   public R2Jesu_IntakeSubsystem() {
-    // Query some boolean state, such as a digital sensor.
-    
+    // constructor
+    SparkMaxConfig wheelsConfig=new SparkMaxConfig();
+    wheelsConfig.encoder.positionConversionFactor(1).velocityConversionFactor(1);
+
+    wheelsConfig.closedLoop
+    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+    .p(.0002)
+    .i(0.0)
+    .d(0.0)
+    .outputRange(-1, 1);
+
+    intake_wheelsMotor.configure(wheelsConfig, 
+    ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+  // Create a Shuffleboard number slider on the "Intake" tab for runtime tuning.
+  // Range 0..9000, initial value from intake_lower_wheel_rpm.
+  intakeRpmEntry = Shuffleboard.getTab("Intake")
+    .add("Intake RPM", intake_lower_wheel_rpm)
+    .withWidget(BuiltInWidgets.kNumberSlider)
+    .withProperties(Map.of("min", 0, "max", 6000))
+    .getEntry();
+ 
   }
 
 
@@ -61,7 +99,7 @@ public class R2Jesu_IntakeSubsystem extends SubsystemBase {
 
    public boolean isIntakeRaised() {
     // If Intake is up, true. 
-    if (intake_armEncoder.getDistance() < 1){
+    if (intake_armEncoder.getDistance() < 100){
        return true;
     }
     else {
@@ -72,41 +110,66 @@ public class R2Jesu_IntakeSubsystem extends SubsystemBase {
     // lowers the intake arm to the floor and starts intake
     //  TO DO: add checks to make sure robot is not against a wall
    public void lowerIntake() {
-    targetPosition=1;
-    intake_wheelsMotor.set(.5);
+    if(isIntakeRaised())
+    {
+      targetPosition=1;
+    }
   } 
 
   // raise the arm, stop wheels
   //  TO DO: add checks to make sure robot is not against a wall
   public void raiseIntake() {
-    targetPosition=0;
-    intake_wheelsMotor.set(0);
+    if(intake_armEncoder.getDistance() < 1){
+    }
+    else{
+      targetPosition=0;
+    }
   }  
 
   // provided for error condition to stop intake if needed
   public void stopIntakeWheels() {
-    intake_wheelsMotor.set(0);
+      setIntakeWheelsVelocity(0.0);
   } 
  
   // moves the intake arm at designated speed
   public void moveArm(double speed) {
-    intake_armMotor.set(speed);
+      intake_armMotor.set(speed);
   } 
+
+  /**
+   * Set intake wheel target velocity (RPM) using the closed-loop controller.
+   * This uses the SparkClosedLoopController.setReference API.
+   * Confirm ControlType and ArbFFUnits names if your REV library version differs.
+   */
+  public void setIntakeWheelsVelocity(double rpm) {
+    // attempt to set closed-loop velocity reference. If your REV API
+    // has a different ControlType name, update it accordingly.
+    // Log the requested setpoint for debugging
+    SmartDashboard.putNumber("intakeWheelTargetRpm", rpm);
+
+    intake_wheelController.setSetpoint(rpm, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
+  }
+
  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("encoderdistance", intake_armEncoder.getDistance());
+    SmartDashboard.putBoolean("isIntakeRaised", isIntakeRaised());
     down_pidOutput = m_armDownController.calculate(intake_armEncoder.getDistance(),intake_armPositions[targetPosition]);
     up_pidOutput = m_armUpController.calculate(intake_armEncoder.getDistance(),intake_armPositions[targetPosition]); 
+    rpm = intakeRpmEntry != null ? intakeRpmEntry.getDouble(intake_lower_wheel_rpm) : intake_lower_wheel_rpm;
+          
 
     if (targetPosition ==0)
     {
         this.moveArm(up_pidOutput);
+        setIntakeWheelsVelocity(0.0);
     }
     else
     {
       this.moveArm(down_pidOutput);
+      setIntakeWheelsVelocity(rpm);
     } 
   }
 
--- src/main/java/frc/robot/subsystems/R2Jesu_ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/R2Jesu_ShooterSubsystem.java
@@ -5,8 +5,21 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import java.util.Map;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -15,10 +28,23 @@ public class R2Jesu_ShooterSubsystem extends SubsystemBase {
  private SparkMax shooterMotor = new SparkMax(52, MotorType.kBrushless);
  private SparkMax kickerbarMotor = new SparkMax(53, MotorType.kBrushed);
   /** Creates a new R2Jesu_ShooterSubsystem. */
+ private final SparkClosedLoopController shooterController = shooterMotor.getClosedLoopController();
+
 
   public R2Jesu_ShooterSubsystem() {
     // Query some boolean state, such as a digital sensor.
-    
+    SparkMaxConfig shooterConfig=new SparkMaxConfig();
+    shooterConfig.encoder.positionConversionFactor(1).velocityConversionFactor(1);
+
+    shooterConfig.closedLoop
+    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+    .p(.0002)
+    .i(0.0)
+    .d(0.0)
+    .outputRange(-1, 1);
+
+    shooterMotor.configure(shooterConfig, 
+    ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
   }
 
 
@@ -32,7 +58,7 @@ public class R2Jesu_ShooterSubsystem extends SubsystemBase {
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
         () -> {
-          shooterMotor.set(0.5);
+          shooterController.setSetpoint(3000, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
           /* one-time action goes here */
         });
   }
@@ -40,7 +66,7 @@ public class R2Jesu_ShooterSubsystem extends SubsystemBase {
   
 
   public void runShooter(double speed) {
-    shooterMotor.set(speed);
+    shooterController.setSetpoint(speed, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
     if(speed > 0.0) {
       kickerbarMotor.set(-1.0);
     } else {
