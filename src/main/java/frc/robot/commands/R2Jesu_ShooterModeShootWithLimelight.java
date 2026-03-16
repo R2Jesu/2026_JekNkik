35e7a52344d43730195fd06e8d040d43cc5f31fb
R2Jesu <r2Jesu2019@gmail.com>
2026-03-16 15:50:34 -0500

    the moveit and good tags changes from sunday night

--- src/main/java/frc/robot/commands/R2Jesu_ShooterModeShootWithLimelight.java
+++ src/main/java/frc/robot/commands/R2Jesu_ShooterModeShootWithLimelight.java
@@ -125,6 +125,10 @@ public class R2Jesu_ShooterModeShootWithLimelight extends Command {
     goodTags.add(27.0);
     goodTags.add(9.0);
     goodTags.add(10.0);
+    goodTags.add(11.0);
+    goodTags.add(8.0);
+    goodTags.add(5.0);
+    goodTags.add(2.0);
 
     LimelightHelpers.SetIMUAssistAlpha(Constants.kLimelightName, .01);
 
