65bee1d17b4c797ca3ef6cc15f659085142145f2
R2Jesu <r2Jesu2019@gmail.com>
2026-03-19 12:59:22 -0500

    aaa

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
     public static final double kDefaultShootSpeed = 3000.0;
     public static final PathConstraints teleopConstraints = new PathConstraints(
         2.12, 1.8, 45, 75);
-    public static final Pose2d kRightHang = new Pose2d(1.02, 4.69, Rotation2d.fromDegrees(180));
-    public static final Pose2d kLeftHang = new Pose2d(1.06, 3.03, Rotation2d.fromDegrees(0));
+    public static final Pose2d kRightHang = new Pose2d(1.15, 5.131, Rotation2d.fromDegrees(180));
+    public static final Pose2d kLeftHang = new Pose2d(1.785, 3.880, Rotation2d.fromDegrees(0));
     public static final Pose2d moveit = new Pose2d(2.70, 1.77, Rotation2d.fromDegrees(0));
     
     public static final double default_armSpeed = .25;
