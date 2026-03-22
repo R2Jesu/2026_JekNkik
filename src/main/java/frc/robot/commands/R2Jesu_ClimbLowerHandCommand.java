5ba8591bfcb48db3606d07032787c5ba7d2524bc
Chris Toney <toney_c@sbcglobal.net>
2026-03-22 10:43:10 -0500

    Ran linter/formatter and cleaned everything currently unused

--- src/main/java/frc/robot/commands/R2Jesu_ClimbLowerHandCommand.java
+++ src/main/java/frc/robot/commands/R2Jesu_ClimbLowerHandCommand.java
@@ -2,54 +2,46 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot.commands;
 
-
-import frc.robot.subsystems.R2Jesu_ClimberSubsystem; //replace with the subsytem(s) needed for your command
 import edu.wpi.first.wpilibj2.command.Command;
-
+import frc.robot.subsystems.R2Jesu_ClimberSubsystem;
 
 /** An example command that uses an example subsystem. */
 public class R2Jesu_ClimbLowerHandCommand extends Command {
-    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-    private final R2Jesu_ClimberSubsystem m_subsystem;
-// Extends the robot's arm.
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final R2Jesu_ClimberSubsystem m_subsystem;
 
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
-     */
-    public R2Jesu_ClimbLowerHandCommand(R2Jesu_ClimberSubsystem subsystem) {
-        m_subsystem = subsystem;
-        // Use addRequirements() here to declare subsystem dependencies for each subsytem used
-        addRequirements(subsystem);
-    }
+  // Extends the robot's arm.
 
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public R2Jesu_ClimbLowerHandCommand(R2Jesu_ClimberSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies for each subsytem used
+    addRequirements(subsystem);
+  }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-        m_subsystem.climbDown();
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_subsystem.climbDown();
+  }
 
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-    }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
-
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-    }
-
-
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 }
--- src/main/java/frc/robot/subsystems/R2Jesu_ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/R2Jesu_ShooterSubsystem.java
@@ -4,11 +4,6 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
-import java.util.Map;
-
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.ClosedLoopSlot;
@@ -16,38 +11,36 @@ import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
 public class R2Jesu_ShooterSubsystem extends SubsystemBase {
- private SparkMax shooterMotor = new SparkMax(52, MotorType.kBrushless);
- private SparkMax kickerbarMotor = new SparkMax(53, MotorType.kBrushed);
-  /** Creates a new R2Jesu_ShooterSubsystem. */
- private final SparkClosedLoopController shooterController = shooterMotor.getClosedLoopController();
+  private SparkMax shooterMotor = new SparkMax(52, MotorType.kBrushless);
+  private SparkMax kickerbarMotor = new SparkMax(53, MotorType.kBrushed);
 
+  /** Creates a new R2Jesu_ShooterSubsystem. */
+  private final SparkClosedLoopController shooterController =
+      shooterMotor.getClosedLoopController();
 
   public R2Jesu_ShooterSubsystem() {
     // Query some boolean state, such as a digital sensor.
-    SparkMaxConfig shooterConfig=new SparkMaxConfig();
+    SparkMaxConfig shooterConfig = new SparkMaxConfig();
     shooterConfig.encoder.positionConversionFactor(1).velocityConversionFactor(1);
 
-    shooterConfig.closedLoop
-    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-    .p(.001)
-    .i(0.0000007)
-    .d(0.00001)
-    .outputRange(0, 1);
+    shooterConfig
+        .closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .p(.001)
+        .i(0.0000007)
+        .d(0.00001)
+        .outputRange(0, 1);
 
-    shooterMotor.configure(shooterConfig, 
-    ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    shooterMotor.configure(
+        shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
   }
-
 
   /**
    * R2Jesu_Shooter command factory method.
@@ -59,16 +52,14 @@ public class R2Jesu_ShooterSubsystem extends SubsystemBase {
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(
         () -> {
-          //shooterController.setSetpoint(3000, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
+          // shooterController.setSetpoint(3000, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
           /* one-time action goes here */
         });
   }
 
-  
-
   public void runShooter(double speed) {
     shooterController.setSetpoint(speed, ControlType.kVelocity, ClosedLoopSlot.kSlot0);
-    if(speed > 0.0) {
+    if (speed > 0.0) {
       kickerbarMotor.set(-1.0);
     } else {
       kickerbarMotor.set(0.0);
@@ -76,12 +67,8 @@ public class R2Jesu_ShooterSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Subsystem shoot speed", speed);
   }
 
- 
-
   @Override
-  public void periodic() {
-   
-  }
+  public void periodic() {}
 
   @Override
   public void simulationPeriodic() {
